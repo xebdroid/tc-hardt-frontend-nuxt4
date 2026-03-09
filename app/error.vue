c10c3d4ef4abe3bbccfd2bfedcf23a2bdc530b5b
Martin Wollny <martin.wollny@joinmedia.de>
2026-03-09 12:21:05 +0100

    One more error fix

--- app/error.vue
+++ app/error.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { NuxtError } from '#app'
-import Error from '~/components/base/BaseError.vue'
+import Error from '~/components/base/Error.vue'
 
 const props = defineProps({
   error: Object as () => NuxtError
